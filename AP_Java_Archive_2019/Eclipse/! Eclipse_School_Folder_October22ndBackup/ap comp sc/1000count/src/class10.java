3725cbc7fde8acf445f3e481b482c7a9e95da54e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 16:57:14 -0700

    class10.java
    The class10 Java file for this directory.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/1000count/src/class10.java
@@ -0,0 +1,15 @@
+public class class10 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+}
+		public static void main(String args) {
+			int n = 0;
+		for(int i=1;i<=10;i++){
+	        for (int x = 1; x<= 10; x++) {
+	        	for(int u=1;u<=10;u++){
+	        		n = n+1;
+	        		System.out.println(n);
+}}}}}
+/*Attempt 1: 
+ * for (int i = 1; i <= 4; i++){
+ * System.out.println (i + 250);*/
