d33f532d02ab4cebe15d1a765b919453712a35d1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 19:33:57 -0700

    Pokemon_Param.java
    The Pokemon parameters Java file for this project.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Pokeman parameters/src/Pokemon_Param.java
@@ -0,0 +1,33 @@
+/* Assignment Info
+A Pokémon's base stat values will most often have the greatest influence over their specific stats at any level. 
+A level 100 Pokémon's stats in Attack, Defense, Speed, Special Attack, and Special Defense will be exactly 5 more than double its base stat values in each.
+The Hit Points (HP) stat will be 110 plus double the base stat value.
+Write a program that returns a Pokémon’s stats for Attack and HP at level 100.
+You should use parameters and methods that return values for this program. 
+You may choose to use the base stats for Blastoise OR Raichu given here:
+Blastoise 
+HP: 79
+Attack: 83
+Defense: 100
+Special Attack: 85
+Special Defense: 105
+Speed: 78
+Raichu
+HP: 60
+Attack: 90
+Defense: 55
+Special Attack: 90
+Special Defense: 80
+Speed: 110
+*/
+public class Pokemon_Param {
+	public static void main(String[] args) {
+		return blastoise();
+		return raichu();
+	public static void blastoise (h = double)); {
+		for (h = 79; h <= h; h++);
+}}} // end of program
+/* output
+
+*/
+// total lines of code: 33 // total size: 0000 bytes (00.00 Kilobytes) \\
