5f3948e667a9b1cd85d1f18c5fb1de286b91d911
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 20:23:27 -0700

    Egg.java
    The Java file for this project.

---
+++ AP_Java_Archive_2019/! Practice-It Java Workstation/CH1/BJP4Exercise1.9Egg/src/Egg.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+/**
+ * @author Sean_Walla_Walla
+ *
+ */
+public class Egg {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// BJP4 Exercise 1.9: Egg
+		// Language/Type: [] Java classes escape sequences method basics println
+		// Author: Marty Stepp (on 2016/09/08)
+		/* Write a complete Java program in a class named Egg that displays the
+		 * following output:
+		 */
+		
+		/*
+  _______
+ /       \
+/         \
+-"-'-"-'-"-
+\         /
+ \_______/
+		 */ // Type your solution here:
+		        System.out.println ("  _______   ");
+		        System.out.println (" /       \\  ");
+		        System.out.println ("/         \\ ");
+		        System.out.println ("-\"-'-\"-'-\"- ");
+		        System.out.println ("\\         / ");
+		        System.out.println (" \\_______/  ");
+}} // end of program
+/* output
+
+  _______
+ /       \
+/         \
+-"-'-"-'-"-
+\         /
+ \_______/
+
+*/
+// total lines of code: 46 // Total size: 1044 bytes (01.04 Kilobytes) \\ 
