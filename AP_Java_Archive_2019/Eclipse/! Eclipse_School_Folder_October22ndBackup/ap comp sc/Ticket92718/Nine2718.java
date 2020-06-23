70799cb4149bbaed467e38a9404ed9c5fe1d7b8a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 19:58:35 -0700

    Nine2718.java
    The source code of the exit ticket for Java class for September 27th 2018.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Ticket92718/Nine2718.java
@@ -0,0 +1,21 @@
+
+public class Nine2718 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for( int i=1; i<=3; i++) {
+		    for(int j=1; j<=3; j++) {
+		        System.out.println((i*j) + " ");
+		        
+		        
+		    }
+		}
+
+		/*| i | j |  output  |
+		 *|---+---+----------/
+		 *|1  | 1 | 1        |\
+		 *|   |   |          */
+	}
+
+}
