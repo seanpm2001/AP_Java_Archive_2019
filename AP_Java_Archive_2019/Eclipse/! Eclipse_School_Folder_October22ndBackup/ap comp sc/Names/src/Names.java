eb5499b1e859bd3277784aaba973b8080bb8a739
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 17:55:24 -0700

    Names.java
    A Java program that lists names. I think it is incomplete.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Names/src/Names.java
@@ -0,0 +1,13 @@
+
+public class Names {
+	{
+	
+	//system.out.printIn("Firstname Lastname")
+
+	System.out.println ("Firstname Lastname");
+	System.out.println ("Firstname Middlename Lastname");
+	System.out.println ("Lastname Firstname");
+	System.out.println ("Lastname Middlename Firstname");
+	
+	}
+}
