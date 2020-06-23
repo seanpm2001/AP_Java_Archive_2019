1c846357078b6446d531972eb8ecb417e1a2cb92
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 17:08:29 -0700

    nine282018.java
    A class project from September 28th 2018.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/9282018/src/nine282018.java
@@ -0,0 +1,22 @@
+public class nine282018 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			/* method 1*/
+				System.out.println ("|'''''''''|");
+				System.out.println (" \\:::::::/");
+				System.out.println ("  \\:::::/");
+				System.out.println ("   \\:::/");
+				System.out.println ("    \\:/");
+				System.out.println ("    /:\\");
+				System.out.println ("   /:::\\");
+				System.out.println ("  /:::::\\");
+				System.out.println (" /:::::::\\");
+				System.out.println ("|'''''''''|");	
+			/*method 2 (failed)
+			 	*c = (":");
+			 	*String line = ("|''''''''''|");
+			 	*String 
+			 	*System;out.println (line,;);
+			 	*System.out.println (c + c + c);
+			 	*/
+}}
