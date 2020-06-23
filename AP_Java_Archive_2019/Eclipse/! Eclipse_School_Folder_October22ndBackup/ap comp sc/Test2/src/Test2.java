998f19da331e267f4d8e0d4a645f78495500b274
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 19:54:56 -0700

    Test2.java
    The Java source file for the 2nd test.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Test2/src/Test2.java
@@ -0,0 +1,105 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greetiiiiiiing = "Hello, world.";
+		String g = "Hello, world.";
+		System.out.println("System.out.println(" + g + ")");
+		System.out.println(");");
+		int myAge = 17;
+		int yourAge = 16;
+		int dogsAge = 7;
+		System.out.println("My age is " + myAge + " and your age is " + yourAge + " and the dog's age is " + dogsAge);
+
+		
+		int variableOne = 1;
+		int variableTwo = 4;
+		int variableThree = variableOne + variableTwo;
+		variableOne = variableThree + 2;
+		System.out.println (variableOne + " " + variableTwo + " " + variableThree);
+		
+
+		int candies = 70;
+		int studentsInClass = 15;
+		int candiesPerStudent = candies / studentsInClass;
+		int candiesLeftOver = candies % studentsInClass;
+		System.out.println (candies + " " + studentsInClass + " " + candiesPerStudent + " " + candiesLeftOver);
+
+		int a = 10;
+
+		int b = 100;
+
+		int c = 1000;
+
+		int d = a + b + c;
+
+		int e = d - a;
+
+		int f = 10 * e;
+
+		System.out.println (a + " " + b + " " + c + " " + d + " " + e + " " + f );
+		
+		int color;
+
+		int classesPerDay = 0;
+
+		System.out.println (classesPerDay);
+		
+		System.out.println ((int) 0.85 + (int) 0.75 + (int) 0.99);
+		
+		System.out.println("\\\\");
+		System.out.println("'");
+		System.out.println("\ta\tb\tc");
+		System.out.println("\"\"\"");
+		System.out.println("C:\nin\the downward spiral");
+	
+		System.out.println (" ");
+		
+			System.out.println ("Dear \"DoubleSlash\" magazine, ");
+			System.out.println();
+			System.out.println ("\tYour publication confuses me. Is it");
+			System.out.println ("a \\\\ slash or a //// slash?");
+			System.out.println ("\nSincerely, ");
+			System.out.println ("Susan \"Suzy\" Smith");
+			
+			System.out.println (" ");
+			
+			/*System.out.println ("This is a test of your/nknowledge of \"quotes"\" used/nin "\'string literals.'\"/n/nYou're bound to "\"get it right"\"/nif you read the section on/n"\'\'\quotes.\'\'\"")
+			 *System.out.println ("This is a test of your/nknowledge of /"/"quotes"/"/");
+			 */
+		
+			System.out.println ("+----+");
+			for (int i = 1; i <= 3; i++) {
+				System.out.println ("\\    /");
+				System.out.println ("/    \\");
+			}
+			System.out.println ("+----+");
+			
+			for (int i = 1; i <= 3; i++)
+				System.out.println ("How many lines");
+				System.out.println ("Are printed?"); }
+				
+				
+			    public static void main(String args) {
+			        for (int i = 2; i <= 2; i++) {
+			            System.out.println("2 times " + 1 * 1 + " = " + i * 1 + " ");
+			            
+			           
+			         
+			            
+			        }
+			    }
+			    {
+
+				
+		}
+	
+
+			
+	
+
+}
+	
+
+
