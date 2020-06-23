5c00ba0eb658977d6fa573acc74744ff22db4d56
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 17:45:15 -0700

    Input101.java
    The input Java project, where I started to learn how to take input in Java.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Input100/src/Input101.java
@@ -0,0 +1,28 @@
+import java.io.Console;
+/**
+ * 
+ */
+/**
+ * @author 0807111
+ *
+ */
+public class Input101 {
+	/**
+	 * 
+	 */
+	public Input101() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		consolMain();
+	}
+		public static void consolMain() {
+		        String(name); ("What is your name? ");
+		        System.out.println("My name is\n",(name));
+	}
+}
