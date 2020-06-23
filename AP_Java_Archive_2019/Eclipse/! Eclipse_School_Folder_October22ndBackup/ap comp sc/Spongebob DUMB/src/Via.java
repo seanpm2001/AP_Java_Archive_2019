13fa66616e9150dfd0c2bd83d6153192540e667e
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 19:48:09 -0700

    Via.java
    The Java source file for this project.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Spongebob DUMB/src/Via.java
@@ -0,0 +1,18 @@
+
+public class Via {
+	public static void Spongebob() {
+		System.out.println ("well it might be stupid");
+	}
+	public static void squidward() {
+		System.out.println ("But it's also");
+	}
+	public static void patrick() {
+		System.out.println ("Dumb");
+	}
+	public static void main (String[] args){
+		Spongebob();
+		squidward();
+		patrick();
+
+	}
+}
