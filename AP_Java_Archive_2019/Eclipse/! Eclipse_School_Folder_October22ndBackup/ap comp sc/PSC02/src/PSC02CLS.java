ac8ee5877d7a828d6e95aea85a35a32f2c7e1a43
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 19:38:06 -0700

    PSC02CLS.java
    The Java project script file for this project.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/PSC02/src/PSC02CLS.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ *
+/**
+ * @author 0807111
+ *
+ */
+public class PSC02CLS {
+	/**
+	 * @param args
+	 */
+		/* Program example
+		 * System.out.println (" |'''''''''''''''''| ");
+		 * System.out.println ("  \\ : : : : : : : / ");
+		 * System.out.println ("   \\ : : : : : : /");
+		 * System.out.println ("    \\ : : : : : /");
+		 * System.out.println ("     \\ : : : : / ");
+		 * System.out.println ("      \\ : : : /");
+		 * System.out.println ("       \\ : : /");
+		 * System.out.println ("        \\ : /");
+		 * System.out.println ("        / : \\");
+		 * System.out.println ("       / : : \\");
+		 * System.out.println ("      / : : : \\");
+		 * System.out.println ("     / : : : : \\");
+		 * System.out.println ("    / : : : : : \\");
+		 * System.out.println ("   / : : : : : : \\");
+		 * System.out.println ("  / : : : : : : : \\");
+		 * System.out.println (" |'''''''''''''''''| ");
+		 */
+/* debug later */
+	public static void main(String[] args) {
+
+		final int linnum = 2;
+		
+		public static final; lslope (String[]) {
+			System.out.print ("\\");
+	}
+		public static final (String[]) {
+			System.out.print("/");
+	}
+		public static final (String[]) {
+			System.out.print (":");
+	}
+		public static final (String[]) {
+			System.out.println ("|''");
+	}
+		public static final (String[]) {
+			System.out.println ("''|");
+	}
+		public static final (String[]) {
+			for int(lslope = linnum; lslope <= linnum; lslope++;);
+	}		
+}}
