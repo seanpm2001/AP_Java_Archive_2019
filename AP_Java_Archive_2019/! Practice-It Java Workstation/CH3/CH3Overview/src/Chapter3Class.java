92318a5ff402967a4e742366e3e698b3cb4a41e0
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 18:08:58 -0700

    Chapter3Class.java
    The chapter 3 overview Java file.

---
+++ AP_Java_Archive_2019/! Practice-It Java Workstation/CH3/CH3Overview/src/Chapter3Class.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * @author Sean_Walla_Walla
+ *
+ */
+public class Chapter3Class {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Overview of Practice-it! Unit 4 chapter 3");
+		System.out.println ("Select a project to begin");
+		
+	public static void projectlist; {
+		System.out.println ("Project List");
+		System.out.println ("Self Check 3.1");
+}}} // end of program
+// total lines of code: 20 // total size: 00000 bytes (000.00 Kilobytes) \\
