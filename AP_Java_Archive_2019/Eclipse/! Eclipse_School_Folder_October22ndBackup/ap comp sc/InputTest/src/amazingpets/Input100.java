607dd408f358a83823ed535ca700e24d934509d6
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 17:50:01 -0700

    Input100.java
    The input 100 Java file for this directory.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/InputTest/src/amazingpets/Input100.java
@@ -0,0 +1,31 @@
+package amazingpets;
+import java.io;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+/**
+ * @author 0807111
+ *
+ */
+public class Input100 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte amazingpets;
+		import java.io.Console;
+
+		public class AmazingPets {
+
+		    public static void main(String[] args) {
+		        Console console = System.console();
+		        String firstName = console.readLine("What is your name? ");
+		        console.printf("My name is %s.\n",firstName);
+		    } 
+		}
+		Scanner sc = new Scanner(System.in);
+		String read = sc.nextLine();
+}}
