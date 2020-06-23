c71394829e36f619d32c63fdfa0045cca2c0e806
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 17:02:47 -0700

    Mantra.java
    The Mantra Java file for this directory.

---
+++ AP_Java_Archive_2019/! Practice-It Java Workstation/CH1/BJP4Exercise1.7Mantra/src/Mantra.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+/**
+ * @author Sean_Walla_Walla
+ *
+ */
+public class Mantra {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// BJP4 Exercise 1.7: Mantra
+		// Language/Type: [] Java method basics println redundancy
+		// Author: Marty Stepp (on 2016/09/08)
+		/* Wirte a complete Java program in a class named Mantra that produces the
+		 * following output. Remove its redundancy by adding a method.
+		 */
+		
+		/* There's ne thing every coder must understand:
+		 * The System.out.println command.
+		 * 
+		 * There's ne thing every coder must understand:
+		 * The System.out.println command.
+		 * 
+		 */ // Type your solution here:
+		//public class Mantra {
+		    public static void oneThing(); {
+		        System.out.println ("There's one thing every coder must understand:");
+		        System.out.println ("The System.out.println command.");
+		    }
+		// public static void main (String[] args) {
+		        oneThing();
+		        System.out.println();
+		        oneThing();
+}} // end of program
+/* output
+
+*/
+// total lines of code: 00 // total size: 0000 bytes (00.00 kilobytes) \\
