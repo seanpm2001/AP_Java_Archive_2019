735d4f1d64d6cfc812dd2a5b127fe3eea890fbb0
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 20:02:18 -0700

    Worksheet27.java
    The Java source file for the 27th worksheet for this class.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/W27/src/Worksheet27.java
@@ -0,0 +1,33 @@
+
+public class Worksheet27 {
+
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub
+	}
+			public static void fancyMethod() {
+				int d = 7;
+			System.out.println("d is " + d);
+			}
+			
+		public static void main(String[] args) {
+				int c = 10;
+			fancyMethod();
+			int b = 100;
+			System.out.println("c is " + c);
+			anotherFancyMethod();
+			System.out.println("b is " + b);
+		}
+
+		public static void anotherFancyMethod() {
+			int a = 1;
+			for (int i = 0; i < 3; i++) {
+				int e = 42;
+				System.out.println("e is " + e);
+			}
+			System.out.println("a is " + a);
+			}
+	
+
+	}
+
+
