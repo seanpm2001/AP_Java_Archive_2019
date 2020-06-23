44651d1d538119968455479fb7b72e365777d04b
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 19:44:34 -0700

    2 Java files
    The 2 Java files for this project.

---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Pseudocode challenge 2/src/PSC2.java
@@ -0,0 +1,21 @@
+public class PSC2 {
+public static void main(String[] args) {
+	System.out.println (" |'''''''''''''''''| ");
+	System.out.println ("  \\ : : : : : : : / ");
+	System.out.println ("   \\ : : : : : : /");
+	System.out.println ("    \\ : : : : : /");
+	System.out.println ("     \\ : : : : / ");
+	System.out.println ("      \\ : : : /");
+	System.out.println ("       \\ : : /");
+	System.out.println ("        \\ : /");
+	System.out.println ("        / : \\");
+	System.out.println ("       / : : \\");
+	System.out.println ("      / : : : \\");
+	System.out.println ("     / : : : : \\");
+	System.out.println ("    / : : : : : \\");
+	System.out.println ("   / : : : : : : \\");
+	System.out.println ("  / : : : : : : : \\");
+	System.out.println (" |'''''''''''''''''| ");
+	final int linnum = 10;
+	
+}}
---
+++ AP_Java_Archive_2019/Eclipse/! Eclipse_School_Folder_October22ndBackup/ap comp sc/Pseudocode challenge 2/src/main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author 0807111
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
