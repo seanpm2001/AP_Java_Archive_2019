2962f8af5226687fc43154eb96bb5196b6af64e1
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 20:17:58 -0700

    Stewie2.java
    The Java file for this exercise.

---
+++ AP_Java_Archive_2019/! Practice-It Java Workstation/CH1/BJP4Exercise1.8Stewie2/src/Stewie2.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+/**
+ * @author Sean_Walla_Walla
+ *
+ */
+public class Stewie2 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// BJP4 Exercise 1.8: Stewie2
+		// Language/Type: [] Java classes escape sequences method basics println
+		// redundancy
+		// Author: Marty Stepp (on 2016/09/08)
+		// Write a complete Java program in a class named Stewie2 that prints the 
+		// following output. Use at least one static method besides main to remove
+		// redundancy.
+		
+		/*
+ 
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+	  
+		*/ // Type your solution here
+		        first();
+		        victory();
+		        last();
+		        victory();
+		        last();
+		        victory();
+		        last();
+		        victory();
+		        last();
+		        victory();
+		        last();
+		    }
+		    public static void first() {
+		        System.out.println("//////////////////////");
+		    }
+		    public static void victory() {
+		        System.out.println("|| Victory is mine! ||");
+		    }
+		    public static void last() {
+		       System.out.println ("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+}} // end of program
+/* output
+
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+
+*/
+// total lines of code: 73 // total size: 1781 bytes (01.81 Kilobytes) \\
