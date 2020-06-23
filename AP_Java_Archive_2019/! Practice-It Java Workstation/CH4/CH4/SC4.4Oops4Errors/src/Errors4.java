3d7b84949b24d259aa84feb87987e69fe4006ddd
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-22 18:30:16 -0700

    Errors4.java
    The Java script (not JavaScript) file for the errors program in chapter 4.4.

---
+++ AP_Java_Archive_2019/! Practice-It Java Workstation/CH4/CH4/SC4.4Oops4Errors/src/Errors4.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+/**
+ * @author Sean_Walla_Walla
+ *
+ */
+public class Error4 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        int a = 7, b = 42;
+        // int smaller;
+        minimum(a, b);
+        if (smaller = a) {
+            System.out.println("a is the smallest!");
+		// TODO Auto-generated method stub
+		// BJP4 Self-Check 4.4 Oops4Errors
+		// Language/Type: [] Java errors if/else method basics return
+		// Author: Marty Stepp (on 2016/09/08)
+		/* The following program contains 7 errors. Correct the errors and submit a 
+		 * working version of the program. The corrected version of the program should
+		 * produce the following output
+		 */
+		/* a is the smallest! */
+		/* type your solution here:*/
+		/* old version
+public class Oops4 {
+    public static void main(String[] args) {
+        int a = 7, b = 42;
+        minimum(a, b);
+        if {smaller = a} {
+            System.out.println("a is the smallest!");
+        }
+    }
+
+    public static void minimum(int a, int b) {
+        if (a < b) {
+            int smaller = a;
+        } else (a => b) {
+            int smaller = b;
+        }
+        return int smaller;
+    }
+}
+		 */ // type your solution here:
+}}
+		    public static void minimum(int a, int b) {
+		        if (a < b) {
+		            int smaller = a;
+		        } else if (a > b) {
+		            int smaller = b;
+}
+		        return int smaller;
+}} // end of program
+/* output
+
+*/
+// end of script // total lines of code: 19 // total size: 0000 bytes (00.00 Kilobytes) \\
